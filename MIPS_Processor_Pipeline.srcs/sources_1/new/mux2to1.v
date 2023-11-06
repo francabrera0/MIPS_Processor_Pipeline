e333d228b04dc13f27f11cdf61e255a147aac337
francabrera0 <francabrera0@gmail.com>
2023-11-06 19:59:38 -0300

    module mux2to1 implemented and tested

---
+++ MIPS_Processor_Pipeline.srcs/sources_1/new/mux2to1.v
@@ -0,0 +1,34 @@
+module mux2to1 
+#(
+    parameter DATA_LEN = 32
+)
+(
+    //Inputs
+    input [DATA_LEN-1:0] i_muxInputA,
+    input [DATA_LEN-1:0] i_muxInputB,
+    input                i_muxSelector,
+
+    //Outputs
+    output [DATA_LEN-1:0] o_muxOutput
+);
+
+localparam INPUTA = 1'b0;
+localparam INPUTB = 1'b1;
+
+
+reg [DATA_LEN-1:0] r_muxOutput;
+
+always @(*) begin
+    case (i_muxSelector)
+        INPUTA: r_muxOutput = i_muxInputA;
+        INPUTB: r_muxOutput = i_muxInputB;
+        
+        default: r_muxOutput = i_muxInputA; 
+    endcase
+end
+
+
+//Assigns
+assign o_muxOutput = r_muxOutput;
+
+endmodule
