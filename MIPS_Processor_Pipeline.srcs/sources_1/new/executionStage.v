b4ceb4c58264cbe36b70aa1c13ea4185a03de7db
Octavio Ulla <octaula@gmail.com>
2023-11-06 23:51:20 -0300

    Inputs and outputs of executionStage

--- MIPS_Processor_Pipeline.srcs/sources_1/new/executionStage.v
+++ MIPS_Processor_Pipeline.srcs/sources_1/new/executionStage.v
@@ -1,26 +1,23 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/06/2023 11:35:57 PM
-// Design Name: 
-// Module Name: executionStage
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module executionStage(
-
-    );
+    //Data inputs
+    input wire [31:0] i_nextPC,
+    input wire [31:0] i_d1,
+    input wire [31:0] i_d2,
+    input wire [31:0] i_inmediatoEx,
+    input wire [4:0] i_rt,
+    input wire [4:0] i_rd,
+    //Control inputs
+    input wire i_aluSrc,
+    input wire [1:0] i_aluOP,
+    input wire i_regDst,
+    //Data outputs
+    output wire [31:0] o_branchPC,
+    output wire [31:0] o_aluResult,
+    output wire [31:0] o_writeRegister,
+    //Control outputs
+    output wire o_zero
+);
 endmodule
