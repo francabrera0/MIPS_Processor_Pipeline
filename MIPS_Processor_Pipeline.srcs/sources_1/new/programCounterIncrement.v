cf7f65814b830bb3ac3afd10e50f28ba2725c4d9
francabrera0 <francabrera0@gmail.com>
2023-11-08 09:15:53 -0300

    Change programCounterIncrement parameter name

--- MIPS_Processor_Pipeline.srcs/sources_1/new/programCounterIncrement.v
+++ MIPS_Processor_Pipeline.srcs/sources_1/new/programCounterIncrement.v
@@ -1,16 +1,16 @@
 module programCounterIncrement 
 #(
-    parameter DATA_LEN = 32
+    parameter PC_LEN = 32
 )
 (
     //Inputs
-    input wire [DATA_LEN-1:0] i_programCounter,
+    input wire [PC_LEN-1:0] i_programCounter,
 
     //Outputs
-    output wire [DATA_LEN-1:0] o_incrementedProgramCounter
+    output wire [PC_LEN-1:0] o_incrementedProgramCounter
 );
 
-reg [DATA_LEN-1:0] r_incrementedProgramCounter;
+reg [PC_LEN-1:0] r_incrementedProgramCounter;
 
 always @(*) begin
     r_incrementedProgramCounter = i_programCounter + 4;
