100ce77e395ac5af8aa2eaaef9a20d2c908b833f
Octavio Ulla <octaula@gmail.com>
2023-11-09 11:44:13 -0300

    memoryStage

---
+++ MIPS_Processor_Pipeline.srcs/sources_1/new/memoryStage.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+
+module memoryStage#(
+    DATA_LEN = 32
+)(
+    //Data inputs
+    input wire [DATA_LEN-1:0] i_address,
+    input wire [DATA_LEN-1:0] i_writeData,
+    //Control inputs
+    input wire i_memRead,
+    input wire i_memWrite,
+    input wire i_branch,
+    input wire i_zero,
+    //Data outputs
+    output wire [DATA_LEN-1:0] o_readData,
+    //Control outputs
+    output wire o_pcSrc
+);
+    
+    branchControl branchControl(
+        .i_branch(i_branch),
+        .i_zero(i_zero),
+        .o_pcSrc(o_pcSrc)
+    );
+    
+    dataMemory #(
+        .DATA_LEN(DATA_LEN)
+    ) dataMemory(
+        .i_address(i_address),
+        .i_writeData(i_writeData),
+        .i_memRead(i_memRead),
+        .i_memWrite(i_memWrite),
+        .o_readData(o_readData)
+    );
+
+endmodule
