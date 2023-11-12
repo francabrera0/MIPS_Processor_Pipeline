4421434874bd0bae49eabc31b72cf5008cf5d7ea
Octavio Ulla <octaula@gmail.com>
2023-11-11 21:35:56 -0300

    ADDU y SUBU

--- MIPS_Processor_Pipeline.srcs/sources_1/new/cpu/execution/ALU.v
+++ MIPS_Processor_Pipeline.srcs/sources_1/new/cpu/execution/ALU.v
@@ -10,34 +10,17 @@ module ALU #(
     input wire  [DATA_LEN-1 : 0] i_operandB,
     input wire    [OP_LEN-1 : 0] i_opSelector,
     output wire [DATA_LEN-1 : 0] o_aluResult,
-    output wire                  o_zero,
-    output wire                  o_overFlow
+    output wire                  o_zero
 ); 
 
-    localparam ADD = 6'b100000;
-    localparam SUB = 6'b100010;
+    localparam ADDU = 6'b100001;
+    localparam SUBU = 6'b100011;
     localparam AND = 6'b100100;
     localparam OR  = 6'b100101;
     localparam XOR = 6'b100110;
     localparam NOR = 6'b100111;
     localparam SRA = 6'b000011;
     localparam SRL = 6'b000010;
-
-    
-    //Adder Substractor module    
-    wire [DATA_LEN-1 : 0] tempAddSubResult;
-    wire                  tempAddSubOverFlow;  
-    
-    SumadorRestador #(
-        .DATA_LEN(DATA_LEN)    
-    ) SumRes
-    (
-        .i_operandA(i_operandA),
-        .i_operandB(i_operandB),
-        .i_substract(i_opSelector[1]),
-        .o_result(tempAddSubResult),
-        .o_overflow(tempAddSubOverFlow)
-    );
     
     
     reg [DATA_LEN-1 : 0] tempResult;
@@ -46,17 +29,14 @@ module ALU #(
     assign o_aluResult = tempResult;
     
     //Zero flag
-    assign o_zero = & (~ o_aluResult);
-
-    assign o_overFlow = (i_opSelector == ADD | i_opSelector == SUB) ? tempAddSubOverFlow : 0; 
-    
+    assign o_zero = & (~ o_aluResult);   
    
     //Calculation
     always @(*)
         begin
             case(i_opSelector)
-                ADD: tempResult = tempAddSubResult;                
-                SUB: tempResult = tempAddSubResult;
+                ADDU: tempResult = i_operandA + i_operandB;                
+                SUBU: tempResult = i_operandA - i_operandB;
                 
                 AND: tempResult = i_operandA & i_operandB;
                 OR : tempResult = i_operandA | i_operandB;
