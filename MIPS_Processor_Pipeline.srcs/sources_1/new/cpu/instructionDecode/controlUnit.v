85fc0857499ae3fbd35a8fc4f69c2b10ea5ef52b
francabrera0 <francabrera0@gmail.com>
2023-11-16 15:34:47 -0300

    shamt was added in the instructionDecodeStage

--- MIPS_Processor_Pipeline.srcs/sources_1/new/cpu/instructionDecode/controlUnit.v
+++ MIPS_Processor_Pipeline.srcs/sources_1/new/cpu/instructionDecode/controlUnit.v
@@ -9,7 +9,7 @@ module controlUnit
 
     //Outputs
     output wire o_regWrite,
-    output wire o_aluSrc,
+    output wire [1:0] o_aluSrc,
     output wire [1:0] o_aluOp,
     output wire o_branch,
     output wire o_regDest,
@@ -25,7 +25,7 @@ localparam BEQ = 6'b000100;
 localparam NOP = 6'b111111;
 
 reg r_regWrite;
-reg r_aluSrc;
+reg [1:0] r_aluSrc;
 reg [1:0] r_aluOp;
 reg r_branch;
 reg r_regDest;
@@ -34,15 +34,17 @@ reg r_memWrite;
 reg r_memToReg;
 
 reg [OPCODE_LEN-1:0] r_opCode;
+reg r_isShamt;
 
 always @(*) begin
     r_opCode = i_instruction[DATA_LEN-1:DATA_LEN-OPCODE_LEN];
+    r_isShamt = ~(i_instruction[2] || i_instruction[5]);       
 
     case (r_opCode)
         RTYPE: begin
             r_regDest = 1'b1;
             r_aluOp = 2'b10;
-            r_aluSrc = 1'b0;
+            r_aluSrc = (r_isShamt) ? 2'b10 : 2'b00;
             r_branch = 1'b0;
             r_memRead = 1'b0;
             r_memWrite = 1'b0;
@@ -52,7 +54,7 @@ always @(*) begin
         LW: begin
             r_regDest = 1'b0;
             r_aluOp = 2'b00;
-            r_aluSrc = 1'b1;
+            r_aluSrc = 2'b01;
             r_branch = 1'b0;
             r_memRead = 1'b1;
             r_memWrite = 1'b0;
@@ -61,7 +63,7 @@ always @(*) begin
         end
         SW: begin
             r_aluOp = 2'b00;
-            r_aluSrc = 1'b1;
+            r_aluSrc = 2'b01;
             r_branch = 1'b0;
             r_memRead = 1'b0;
             r_memWrite = 1'b1;
@@ -69,7 +71,7 @@ always @(*) begin
         end
         BEQ: begin
             r_aluOp = 2'b01;
-            r_aluSrc = 1'b0;
+            r_aluSrc = 2'b00;
             r_branch = 1'b1;
             r_memRead = 1'b0;
             r_memWrite = 1'b0;
@@ -78,7 +80,7 @@ always @(*) begin
         NOP: begin
             r_regDest = 1'b0;
             r_aluOp = 2'b00;
-            r_aluSrc = 1'b0;
+            r_aluSrc = 2'b00;
             r_branch = 1'b0;
             r_memRead = 1'b0;
             r_memWrite = 1'b0;
@@ -88,7 +90,7 @@ always @(*) begin
         default: begin
             r_regDest = 1'b0;
             r_aluOp = 2'b00;
-            r_aluSrc = 1'b0;
+            r_aluSrc = 2'b00;
             r_branch = 1'b0;
             r_memRead = 1'b0;
             r_memWrite = 1'b0;
