7f4e28d5f02d9e08f5a6c0d0f254ba154a66ad67
Octavio Ulla <octaula@gmail.com>
2023-12-04 18:54:53 -0300

    pcSrc signal

--- MIPS_Processor_Pipeline.srcs/sources_1/new/cpu/memory/branchControl.v
+++ MIPS_Processor_Pipeline.srcs/sources_1/new/cpu/memory/branchControl.v
@@ -5,11 +5,16 @@ module branchControl(
     input wire [1:0] i_branch,
     input wire i_zero,
     //Control outputs
-    output wire o_PCSrc
+    output reg o_PCSrc
 );
 
 //Bit bajo de branch indica si es instruccion de branch
 //Bit alto indica si es equal o not equal
-assign o_PCSrc = i_branch[0] & (i_branch[1] ^ i_zero);
+always @(*) begin
+    case(i_branch)
+        2'b10: o_PCSrc = 1;
+        default: o_PCSrc = i_branch[0] & (i_branch[1] ^ i_zero);       
+    endcase
+end
 
 endmodule
