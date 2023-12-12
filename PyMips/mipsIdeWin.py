3819150ed7d0b906604b7a6c8767614461188656
Octavio Ulla <octaula@gmail.com>
2023-12-12 18:29:36 -0300

    COM9

--- PyMips/mipsIdeWin.py
+++ PyMips/mipsIdeWin.py
@@ -14,7 +14,7 @@ class mipsIDE(QMainWindow):
     assemblyCode = None
     parser = assemblyParser(instructionTable, registerTable, 4)
     programValid = False
-    serialComPort = serial.Serial("COM11", 115200, timeout=2)
+    serialComPort = serial.Serial("COM9", 115200, timeout=2)
 
     def __init__(self):
         super().__init__()
